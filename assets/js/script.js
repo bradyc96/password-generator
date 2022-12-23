3055bb7ec03861dba94d86a6ec0c9fdc43f79c94
Brady Collier <bradycollier.123@gmail.com>
2022-12-22 23:38:48 -0800

    updates

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,30 @@
 // Assignment code here
+// all the variables for each character set
 
+
+
+//logic
+function passwordOptions() {
+    var length = parseInt(prompt("What's the length?(8-128)")); 
+    var hasLower = confirm("Do you want lowercase?");
+    var hasUpper = confirm("Do you want uppercase?");
+    var hasNumbers = confirm("Do you want numbers?");
+    var hasSpecialCharacters = confirm("Do you want special characters?")
+    var options = {}
+}
+
+
+function generatePassword(){
+    var password = [];
+    var options = passwordOptions();{
+    var numericCharacters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+    var lowercaseCharacters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+    var uppercaseCharacters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+    var specialCharacters = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "+", "?", "/", "-", ":", ";", "[", "]", "{", "}", ".", "<", ">", "=", "_", "`", "|", "~"];
+    var selectedArray = [];
+    }
+    return password.join("")
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
